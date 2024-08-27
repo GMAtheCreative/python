ef01503780f5c471503e32e78fe73350d4ea40f2
GMAtheCreative <timothywonder01@gmail.com>
2024-08-27 15:38:49 +0100

    play ground

---
+++ src/listPlayGround/list.py
@@ -0,0 +1,6 @@
+def dictionary(param):
+    return {num:num ** 3 for num in param}
+
+    # for index in param:
+    #     list_dictionary[index] = index ** 3
+    # return list_dictionary
---
+++ test/listTest/list_test.py
@@ -0,0 +1,11 @@
+import unittest
+
+from listPlayGround.list import dictionary
+
+
+class TestList(unittest.TestCase):
+    def test_that_list_in_multiple(self):
+        self.assertEqual(dictionary([1,2,3,4,5]), {1:1,2:8,3:27,4:64,5:125})  # add assertion here
+
+if __name__ == '__main__':
+    unittest.main()
